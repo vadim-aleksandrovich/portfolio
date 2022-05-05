35896ee749eb5a46636732680193a580e704d91b
Vadim Aleksandrovich <vadim.alexandrovich@protonmail.ch>
2022-05-05 16:49:55 +0300

    fix: some links and images, correct css rules

--- js/content/projects.js
+++ js/content/projects.js
@@ -3,7 +3,7 @@ export default [
     title:      "Shelter",
     subtitle:   "Web portal for help to homeless pets",
     date:       "Sep 2020",
-    content:    "My first web project. Technology in use: Adaptive layout, HTML, CSS3, first meeting with Javascript.",
+    content:    "My first web project. Technology in use: Adaptive layout, HTML, CSS3, Javascript.",
     image:      "../../assets/images/projects/shelter.jpeg",
     link:       "https://shelter.aleksandrovich.it/pages/main/main.html",
     sourcecode: "https://github.com/vadim-aleksandrovich/front-end-projects/tree/shelter"
@@ -11,9 +11,9 @@ export default [
 
   {
     title:      "Calculator",
-    subtitle:   "Online Calculator",
+    subtitle:   "Online calculator",
     date:       "Sep 2020",
-    content:    "Technologies in use: HTML, CSS3, Javascript, Algorithms",
+    content:    "Technologies  in use: HTML, CSS3, Javascript, Algorithms",
     image:      "../../assets/images/projects/calculator.jpeg",
     link:       "https://calculator.aleksandrovich.it",
     sourcecode: "https://github.com/vadim-aleksandrovich/front-end-projects/tree/calculator"
@@ -21,9 +21,9 @@ export default [
 
   {
     title:      "Momentum",
-    subtitle:   "Usefull Web Screensaver",
+    subtitle:   "Usefull web screensaver",
     date:       "Oct 2020",
-    content:    "Adaptive Design, work with several api, Browser's Storage, html5, css3, javascript",
+    content:    "Adaptive Design, work with several api, Browser's Storage, HTML5, CSS3, Javascript",
     image:      "../../assets/images/projects/momentum.jpeg",
     link:       "https://momentum.aleksandrovich.it",
     sourcecode: "https://github.com/vadim-aleksandrovich/front-end-projects/tree/momentum"
@@ -31,19 +31,19 @@ export default [
 
   {
     title:      "Keybord",
-    subtitle:   "It may works like a virtual keyboard. And also duplicate physical keys, and shows which one had been pressed",
+    subtitle:   "It may works like a virtual keyboard. Also duplicate physical keys, and shows which one had been pressed",
     date:       "Dec 2020",
     content:    "Browser's Events, Input Tracking",
-    image:      "../../assets/images/projects/keyboard.png",
+    image:      "../../assets/images/projects/keyboard.jpeg",
     link:       "https://keyboard.aleksandrovich.it",
     sourcecode: "https://github.com/vadim-aleksandrovich/front-end-projects/tree/virtual-keyboard"
   },
 
   {
     title:      "Gem Puzzle",
-    subtitle:   "Famous game, where you have to put gems in right order. If you can't win, just click 'autofinish' to see how does it works)",
+    subtitle:   "Famous game, where you need to put the gems in the right order. If you can't do it, just click 'autofinish' to see how it works)",
     date:       "Jan 2021",
-    content:    "Browser's Events, Input Tracking, Browser's Storage, Algorithms, Javascript, CSS3, HTML",
+    content:    "Browser's Events, Browser's Storage, Algorithms, Javascript, CSS3, HTML5",
     image:      "../../assets/images/projects/gem-puzzle.jpeg",
     link:       "https://puzzle.aleksandrovich.it",
     sourcecode: "https://github.com/vadim-aleksandrovich/front-end-projects/tree/gem-puzzle"
@@ -51,9 +51,9 @@ export default [
 
   {
     title:      "English for Kids",
-    subtitle:   "Flash card game for young english learners",
+    subtitle:   "Flash cards for young english learners",
     date:       "Feb 2021",
-    content:    "Browser Events, Input Tracking, Browser's Storage, Media, Algoritms, Javascript, CSS3, HTML5",
+    content:    "Browser Events, Input Tracking, Browser's Storage, Media, Algorithms, Javascript, SASS, HTML5",
     image:      "../../assets/images/projects/english-for-kids.jpeg",
     link:       "https://english.aleksandrovich.it",
     sourcecode: "https://github.com/vadim-aleksandrovich/front-end-projects/tree/english-for-kids"
@@ -61,10 +61,10 @@ export default [
 
   {
     title:      "Portfolio",
-    subtitle:   "My IT Portfolio",
+    subtitle:   "My IT portfolio",
     date:       "Dec 2021",
-    content:    "Javascript, SCSS, HTML5, AWS Route 53, AWS S3, AWS Cloudfront",
-    image:      "",
+    content:    "Javascript, SASS, HTML5, AWS Route 53, AWS S3, AWS Cloudfront",
+    image:      "../../assets/images/projects/portfolio.jpeg",
     link:       "https://aleksandrovich.it",
     sourcecode: "https://github.com/vadim-aleksandrovich/portfolio"
   },
