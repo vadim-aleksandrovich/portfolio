fe556bf535d9383715bbe712bb4c80385cb7b7f7
Vadim Aleksandrovich <vadim.alexandrovich@protonmail.ch>
2022-09-16 23:02:52 +0300

    upd: redesign certificates block

--- src/js/content/certificates.js
+++ src/js/content/certificates.js
@@ -4,7 +4,7 @@ export default [
     subtitle: "THE ROLLING SCOPES SCHOOL",
     date:     "Feb 2021",
     badge:    "./assets/images/certificates/rsschool-js-badge.png",
-    image:    "./assets/images/certificates/rsschool-js-cert.pdf",
+    image:    "./assets/images/certificates/rsschool-js-cert.jpg",
     id:       "0",
   },
 
@@ -13,7 +13,7 @@ export default [
     subtitle: "EF Standart English Test",
     date:     "Dec 2021",
     badge:    "./assets/images/certificates/efset-badge.png",
-    image:    "./assets/images/certificates/efset-cert.pdf",
+    image:    "./assets/images/certificates/efset-cert.jpg",
     id:       "1",
   },
 
@@ -22,7 +22,7 @@ export default [
     subtitle: "Amazon Web Services",
     date:     "Sep 2022",
     badge:    "./assets/images/certificates/aws-learning-object-storage-badge.png",
-    image:    "./assets/images/certificates/aws-learning-object-storage-cert.pdf",
+    image:    "./assets/images/certificates/aws-learning-object-storage-cert.jpg",
     id:       "2",
   },
 
@@ -31,8 +31,7 @@ export default [
     subtitle: "Amazon Web Services",
     date:     "Sep 2022",
     badge:    "./assets/images/certificates/aws-learning-block-storage-badge.png",
-    image:    "./assets/images/certificates/aws-learning-block-storage-cert.pdf",
+    image:    "./assets/images/certificates/aws-learning-block-storage-cert.jpg",
     id:       "3",
-  },
-
+  }
 ]
--- src/js/elements/containers.js
+++ src/js/elements/containers.js
@@ -2,7 +2,7 @@ export default
   {
     experience: `<h2 class="container_title">experience</h2>`,
     education: `<h2 class="container_title">education</h2>`,
-    certificats: `<h2 class="container_title">certificats</h2>`,
+    certificats: `<h2 class="container_title container_title_certificate">certificates</h2>`,
     projects: `<h2 class="container_title">projects</h2>`,
   };
 
