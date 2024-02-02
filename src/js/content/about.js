30f510b13ec02350ce61f651d4db91d04366c143
Vadim Aleksandrovich <alexandrovichvadim@gmail.com>
2024-02-02 15:14:49 +0300

    add: new info

--- src/js/content/about.js
+++ src/js/content/about.js
@@ -1,6 +1,6 @@
 export default {
   title: "Vadim Aleksandrovich",
   subtitle: "DevOps (Cloud) Engineer",
-  content: "Since 2021, I've been working on a variety of projects, from internal initiatives to major commercial developments in fintech and retail. My specialization includes deep knowledge of AWS, Yandex Cloud, and Sber Cloud, as well as expertise in Kubernetes, Docker, and CI/CD processes using GitLab CI and ArgoCD.<br> I spend a lot of time learning and growing, taking courses and getting certificates from big names like Amazon Web Services and Yandex Cloud. I like to share what I know with others in the community and I'm always looking to learn more.",
+  content: "",
   photo: "./assets/images/avatar.jpeg",
 }
